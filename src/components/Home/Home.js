49de4f06d207d832241ed6d7f41ffc3a404a0494
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-10-11 22:30:38 +0600

    added a carousel and responsive home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,24 @@
 
 import { useLoaderData } from 'react-router-dom';
+import ShowCarousel from '../ShowCarousel/ShowCarousel';
 import ShowTopic from '../ShowTopic/ShowTopic';
 import './Home.css'
 const Home = () => {
     const quizeTopic = useLoaderData()
     return (
-        <div style={{padding:'10px'}}>
-            
+        <div style={{ padding: '10px' }}>
+            <div className='home-container'>
+            <ShowCarousel></ShowCarousel>
+            </div>
             <h3 className='topic-header'>Our Practice Topic</h3>
-           <div className='topic-container'>
-           {
-                quizeTopic.data.map(topic => <ShowTopic 
-                key={topic.id}
-                topic = {topic}
-                ></ShowTopic>)
-            }
-           </div>
+            <div className='topic-container'>
+                {
+                    quizeTopic.data.map(topic => <ShowTopic
+                        key={topic.id}
+                        topic={topic}
+                    ></ShowTopic>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/ShowCarousel/ShowCarousel.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import logo1 from '../../image/logo1.jpg'
+import logo2 from '../../image/logo2.jpg'
+import logo3 from '../../image/logo3.jpg'
+function ShowCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item interval={2000}>
+        <img
+          className="d-block w-100"
+          style={{height:'80vh'}}
+          src={logo1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3 style={{color:'tomato'}}>Practice With Us</h3>
+          <p>If you join with us, we will make you perfect coder.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+          className="d-block w-100"
+          style={{height:'80vh'}}
+          src={logo2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3 style={{color:'tomato'}}>Learning Someting New</h3>
+          <p>For shinging in life, gather knowladge</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+          className="d-block w-100"
+          style={{height:'80vh'}}
+          src={logo3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3 style={{color:'tomato'}}>Work Hard</h3>
+          <p>
+            Hard work can change you.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ShowCarousel
