9b7ec84335b1968b9b71d1a355f710680cc129c1
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-10-11 21:03:40 +0600

    all pages setup with layout and design navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './components/Home/Home';
+import Blogs from './components/Blogs/Blogs';
+import Statistics from './components/Statistics/Statistics';
 
 function App() {
   const router = createBrowserRouter([
-    {path:'/', element: <h3>This is default page</h3>}, 
-    {path:'/home', element: <h3>This is home page</h3>}
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path:'/blogs',
+          element: <Blogs></Blogs>
+        },
+        {
+          path:'/statistics',
+          element: <Statistics></Statistics>
+        }
+      ]
+    }
 
   ])
   return (
     <div className="App">
-       <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import './Header.css'
+const Header = () => {
+    return (
+        <Navbar bg="dark" expand="lg">
+          <Container>
+            <Navbar.Brand > 
+                <Link className='brand-name' to={'/'}>Web<span style={{color:'tomato'}}>Art</span></Link>
+            </Navbar.Brand>
+            <Navbar.Toggle style={{background:'white'}} aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto nav-link">
+                <Link to={'/'}>Home</Link>
+                <Link to={'/statistics'}>Statistics</Link>
+                <Link to={'/blogs'}>Blogs</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+    }
+export default Header;
