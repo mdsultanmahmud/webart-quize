4fb941e03aff3bf313e47d63c687723c53db171b
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-10-12 14:47:52 +0600

    check the correct answer and show the messege with toast

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,39 +1,51 @@
 import React from 'react';
 import './Question.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 const Question = ({ SingleTopicQuestion }) => {
     const { correctAnswer, id, options, question } = SingleTopicQuestion
     const regexForStripHTML = /(<([^>]+)>)/gi
     const stripQuestion = question.replaceAll(regexForStripHTML, '')
-    function checkAns(clickedOption){
-       console.log(correctAnswer === clickedOption)
+    let correctMsg
+    function checkAns(clickedOption) {
+        if (correctAnswer === clickedOption) {
+            correctMsg = 'Your answer is correct!!'
+        } else {
+            correctMsg = 'Your answer is wrong!!'
+        }
     }
-
+    const notify = () => toast(correctMsg);
     return (
         <div className='single-question'>
-            <div>
+            
+            <div className='qsn'>
                 <h5>{stripQuestion}</h5>
-                <div className='option'>
-                    {
-                        options.map(option => <DisplayOption
-                            option={option}
-                            id = {id}
-                            checkAns = {checkAns}
-                        ></DisplayOption>)
-                    }
-                </div>
+                <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
             </div>
+            <div className='option'>
+                {
+                    options.map(option => <DisplayOption
+                        option={option}
+                        id={id}
+                        checkAns={checkAns}
+                        notify={notify}
+                    ></DisplayOption>)
+                }
+            </div>
+
         </div>
     );
 };
 
-function DisplayOption({ option,id,checkAns }) {
+function DisplayOption({ option, id, checkAns, notify }) {
     return (
         <div className='single-option'>
-            <input  id={id} type="radio" />
-            <label for={id}>
-            <p onClick={() =>checkAns(option)}  className='ans'>{option}</p>
-            </label>
-            
+            <button className='ans' onClick={notify}>
+                <p onClick={() => checkAns(option)} style={{ margin: '0' }}>{option}</p>
+            </button>
+            <ToastContainer />
         </div>
     )
 }
